69cff090b0a81a22b35c0457afd5f94047881633
“Alina <maiyachka1@gmail.com>
2022-12-13 15:48:45 +0100

    second

--- src/app/product-list.ts
+++ src/app/product-list.ts
@@ -18,7 +18,7 @@ export const products: ProductInterface[] = [
     price: 12,
     description: '1500м',
     teg: 'merino',
-    image: "https://agatha.in.ua/image/cache/catalog/110621/06noyab2021_2/10916-600x600.jpg",
+    image: "https://agatha.in.ua/image/cache/catalog/181021/angora/2b7d3be8-ed80-4d8d-acc5-162670a8bef2-600x600.jpeg",
     id:5
   },
   {
@@ -58,7 +58,7 @@ export const products: ProductInterface[] = [
     price: 40,
     description: '700м',
     teg: 'cash',
-    image: "https://agatha.in.ua/image/cache/catalog/8_fevralya_2022/11308/a339f9ee-72ab-41fc-bab7-9ae13afdf8b4-600x600.jpeg",
+    image: "https://agatha.in.ua/image/cache/catalog/181021/angora/95b186e2-f546-491f-a715-9678d3ea8825-600x600.jpeg",
     id:6
   },
   {
@@ -68,7 +68,7 @@ export const products: ProductInterface[] = [
     price: 40,
     description: '1300м',
     teg: 'cash',
-    image: "https://agatha.in.ua/image/cache/catalog/8_fevralya_2022/11308/a339f9ee-72ab-41fc-bab7-9ae13afdf8b4-600x600.jpeg",
+    image: "https://agatha.in.ua/image/cache/catalog/181021/angora/13a1827b-111f-4e46-8ce7-c264d9ba49b8-900x900.jpeg",
     id:7
   },
   {
@@ -78,7 +78,7 @@ export const products: ProductInterface[] = [
     price: 40,
     description: '700м',
     teg: 'cash',
-    image: "https://agatha.in.ua/image/cache/catalog/8_fevralya_2022/11308/a339f9ee-72ab-41fc-bab7-9ae13afdf8b4-600x600.jpeg",
+    image: "https://agatha.in.ua/image/cache/catalog/18dec2021/raznoe/178ba84d-2844-4f94-8089-0063a057188a-600x600.jpeg",
     id:8
   },
   {
@@ -88,7 +88,7 @@ export const products: ProductInterface[] = [
     price: 12,
     description: '1500м',
     teg: 'merino',
-    image: "https://agatha.in.ua/image/cache/catalog/8sent2020/angora/moher/32365-600x600.jpg",
+    image: "https://agatha.in.ua/image/cache/catalog/18dec2021/raznoe3/2d07375a-6068-4cc2-8d1c-384994e0db49-600x600.jpeg",
     id:9
   },
   {
@@ -98,7 +98,7 @@ export const products: ProductInterface[] = [
     price: 54,
     description: '500м',
     teg: 'angora',
-    image: "https://agatha.in.ua/image/cache/catalog/8sent2020/angora/moher/32365-600x600.jpg",
+    image: "https://agatha.in.ua/image/cache/catalog/110621/5noyab/airone/7085f18e-99b7-4469-8cea-21a989b863b6-600x600.jpeg",
     id:9
   },
   {
